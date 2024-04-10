2cc6b304e3f9d064b4cbe2a782f573f6320350db
hsaoho <121716324+hsaoho@users.noreply.github.com>
2024-04-10 22:09:23 +0800

    提交信息

--- 04_led_driver/led_driver.c
+++ 04_led_driver/led_driver.c
@@ -1,3 +1,23 @@
+#include <linux/module.h>
+
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/miscdevice.h>
+#include <linux/kernel.h>
+#include <linux/major.h>
+#include <linux/mutex.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/stat.h>
+#include <linux/init.h>
+#include <linux/device.h>
+#include <linux/tty.h>
+#include <linux/kmod.h>
+#include <linux/gfp.h>
+
+#include "led_opr.h"
+
+
 
 
 
